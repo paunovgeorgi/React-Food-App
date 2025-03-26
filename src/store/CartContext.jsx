c86b3f74338b4a3958b3d1259f5051e3b0ba4568
paunovgeorgi <133797408+paunovgeorgi@users.noreply.github.com>
2025-03-26 17:28:20 +0200

    Add_ITEM action type implemented

--- src/store/CartContext.jsx
+++ src/store/CartContext.jsx
@@ -9,7 +9,23 @@ const CartContext = createContext({
 function cartReducer(state, action){
     if (action.type === 'ADD_ITEM') {
         // update the state to add meal item
-       
+        const existingCartItemIndex = state.items.findIndex((item) => item.id === action.item.id);
+
+        const updatedItems = [...state.items]
+
+        if (existingCartItemIndex > -1) {
+            const existingItem = state.items[existingCartItemIndex];
+            const updatedItem = {
+                ...existingItem,
+                quantity: existingItem.quantity + 1
+            }
+
+            updatedItems[existingCartItemIndex] = updatedItem;
+        }else{
+            updatedItems.push({ ...action.item, quantity: 1 });
+        }
+
+        return { ...state, items: updatedItems };
     }
 
     if (action.type === 'REMOVE_ITEM') {
