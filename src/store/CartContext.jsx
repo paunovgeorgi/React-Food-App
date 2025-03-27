df088db7e5c9c3887c2c6d45b067a81dfd4bfd50
paunovgeorgi <133797408+paunovgeorgi@users.noreply.github.com>
2025-03-27 11:20:49 +0200

    CartContext updated

--- src/store/CartContext.jsx
+++ src/store/CartContext.jsx
@@ -30,7 +30,7 @@ function cartReducer(state, action){
 
     if (action.type === 'REMOVE_ITEM') {
         // remove an item from the state
-        const existingCartItemIndex = state.items.findIndex((item) => item.id === action.item.id);
+        const existingCartItemIndex = state.items.findIndex((item) => item.id === action.id);
         const existingItem = state.items[existingCartItemIndex];
 
         const updatedItems = [...state.items];
@@ -52,9 +52,22 @@ function cartReducer(state, action){
 export function CartContextProvider({children}){
     const [cart, dispatchCartAction] = useReducer(cartReducer, { items: []})
 
+    function addItem(item){
+        dispatchCartAction({type: 'ADD_ITEM', item: item });
+    }
+
+    function removeItem(id){
+        dispatchCartAction({type: 'REMOVE_ITEM', id: id });
+    }
+
     const cartContext = {
         items: cart.items,
+        addItem,
+        removeItem
     }
+
+    console.log(cartContext);
+    
 
     return <CartContext value={cartContext}>{children}</CartContext>
 }
