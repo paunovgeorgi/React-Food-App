3512b193aee683331c5aa9500245dc8a92b802ce
paunovgeorgi <133797408+paunovgeorgi@users.noreply.github.com>
2025-03-31 11:31:23 +0300

    useHttp hook for handling server requests implemented.

---
+++ src/hooks/useHttp.js
@@ -0,0 +1,43 @@
+import { useCallback, useEffect, useState } from "react";
+
+async function sendHttpRequest(url, config){
+    const response = await fetch(url, config);
+    const result = response.json();
+    if (!response.ok) {
+        throw new Error(result.message || 'Something went wrong...')
+    }
+
+    return result;
+}
+
+export default function useHttp(url, config, initialData){
+    const [data, setData] = useState(initialData);
+    const [isLoading, setIsLoading] = useState(false);
+    const [error, setError] = useState();
+
+    const sendRequest = useCallback(async function sendRequest(data){
+        setIsLoading(true);
+        try {
+            const restData = await sendHttpRequest(url, {...config, body: data});
+            setData(restData);
+        } catch (error) {
+            setError(error.message)
+        }
+
+        setIsLoading(false)
+    }, [url, config]);
+
+    useEffect(() => {
+        if (config && (config.method === 'GET' || !config.method) || !config) {
+            sendRequest();
+        }
+    }, [sendRequest, config])
+
+    return{
+        data,
+        isLoading,
+        error,
+        sendRequest
+    }
+    
+}
